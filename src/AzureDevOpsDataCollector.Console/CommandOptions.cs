6a27d0b72255050f685acbc824df0500d2b9260a
Lila Tran <litra@microsoft.com>
2020-06-06 17:27:27 -0700

    Add each verb for each collector

--- src/AzureDevOpsDataCollector.Console/CommandOptions.cs
+++ src/AzureDevOpsDataCollector.Console/CommandOptions.cs
@@ -5,16 +5,28 @@ namespace AzureDevOpsDataCollector.Console
 {
     public class CommandOptions
     {
+        public ProjectCommandOptions ProjectCommandOptions { get; set; }
+        public RepositoryCommandOptions RepositoryCommandOptions { get; set; }
+    }
+
+    [Verb("project", HelpText = "Collect Azure DevOps projects data")]
+    public class ProjectCommandOptions : CommandOptionsBase
+    {
+    }
+
+    [Verb("repository", HelpText = "Collect Azure DevOps repository data")]
+    public class RepositoryCommandOptions : CommandOptionsBase
+    {
+        [Option("projects", Required = false, Default = null, Separator = ':', HelpText = "A list of project names, default is to collect all projects if not specified. For example: 'project1:project2:project3'")]
+        public IEnumerable<string> Projects { get; set; }
+    }
+
+    public class CommandOptionsBase
+    {
         [Option("account", Required = true, HelpText = "The name of Azure DevOps account, for example: https://dev.azure.com/lilatran where lilatran is the account name")]
         public string Account { get; set; }
 
         [Option("pat", Required = true, HelpText = "Azure DevOps Personal Access Token")]
         public string PersonalAccessToken { get; set; }
-
-        [Option("collector", Required = false, Default = CollectorType.Project, HelpText = "The name of the collector to run.")]
-        public CollectorType CollectorType { get; set; }
-
-        [Option("projects", Required = false, Default = null, Separator = ':', HelpText = "A list of project names, default is to collect all projects if not specified. For example: 'project1:project2:project3'")]
-        public IEnumerable<string> Projects { get; set; }
     }
 }
--- src/AzureDevOpsDataCollector.Console/Program.cs
+++ src/AzureDevOpsDataCollector.Console/Program.cs
@@ -3,6 +3,7 @@ using AzureDevOpsDataCollector.Core.Collectors;
 using CommandLine;
 using CommandLine.Text;
 using EFCore.AutomaticMigrations;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace AzureDevOpsDataCollector.Console
@@ -15,23 +16,36 @@ namespace AzureDevOpsDataCollector.Console
             CommandOptions parsedOptions = Program.ParseArguments(args);
             if (parsedOptions == null) { return -1; }
 
-            // Create AzureDevOps client
-            VssClient vssClientConnector = new VssClient(parsedOptions.Account, parsedOptions.PersonalAccessToken);
-
             // Create DbContext client
             VssDbContext dbContext = new VssDbContext();
             await MigrateDatabaseToLatestVersion.ExecuteAsync(dbContext);
 
-            // Collect Repository data
-            if (parsedOptions.CollectorType == CollectorType.Repository)
+            // Collect Project
+            if (parsedOptions.ProjectCommandOptions != null)
             {
-                RepositoryCollector repositoryCollector = new RepositoryCollector(vssClientConnector, dbContext, parsedOptions.Projects);
-                await repositoryCollector.RunAsync();
-            }
-            else if (parsedOptions.CollectorType == CollectorType.Project)
-            {
-                ProjectCollector collector = new ProjectCollector(vssClientConnector, dbContext, parsedOptions.Projects);
+                string account = parsedOptions.ProjectCommandOptions.Account;
+                string personalAccessToken = parsedOptions.ProjectCommandOptions.PersonalAccessToken;
+
+                // Create AzureDevOps client
+                VssClient vssClient = new VssClient(account, personalAccessToken);
+
+                // Run
+                ProjectCollector collector = new ProjectCollector(vssClient, dbContext);
                 await collector.RunAsync();
+            }
+            // Collect Repository
+            else if (parsedOptions.RepositoryCommandOptions != null)
+            {
+                string account = parsedOptions.RepositoryCommandOptions.Account;
+                string personalAccessToken = parsedOptions.RepositoryCommandOptions.PersonalAccessToken;
+                IEnumerable<string> projects = parsedOptions.RepositoryCommandOptions.Projects;
+
+                // Create AzureDevOps client
+                VssClient vssClientConnector = new VssClient(account, personalAccessToken);
+
+                // Run
+                RepositoryCollector repositoryCollector = new RepositoryCollector(vssClientConnector, dbContext, projects);
+                await repositoryCollector.RunAsync();
             }
 
             // Returns zero on success
@@ -41,15 +55,14 @@ namespace AzureDevOpsDataCollector.Console
         private static CommandOptions ParseArguments(string[] args)
         {
             // Parse command line options
-            Parser parser = new Parser((ParserSettings parserSettings) =>
-            {
-                parserSettings.AutoHelp = false;
-            });
-            ParserResult<CommandOptions> parserResult = parser.ParseArguments<CommandOptions>(args);
+            ParserResult<object> parserResult = Parser.Default.ParseArguments<ProjectCommandOptions, RepositoryCommandOptions>(args);
 
             // Map results after parsing
             CommandOptions commandOptions = new CommandOptions();
-            parserResult.MapResult<CommandOptions, object>((CommandOptions opts) => commandOptions = opts, (errs) => 1);
+            parserResult.MapResult<ProjectCommandOptions, RepositoryCommandOptions, object>(
+                (ProjectCommandOptions opts) => commandOptions.ProjectCommandOptions = opts,
+                (RepositoryCommandOptions opts) => commandOptions.RepositoryCommandOptions = opts,
+                (errs) => 1);
 
             // Return null if not able to parse
             if (parserResult.Tag == ParserResultType.NotParsed) 
@@ -57,8 +70,7 @@ namespace AzureDevOpsDataCollector.Console
                 HelpText helpText = HelpText.AutoBuild(parserResult);
                 helpText.AddEnumValuesToHelpText = true;
                 helpText.AddOptions(parserResult);
-                System.Console.WriteLine(helpText);
-                return null; 
+                commandOptions = null;
             }
 
             // Return list of parsed commands
