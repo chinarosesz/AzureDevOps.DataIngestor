759f46088ca45ce339a36575f36910079d5adbe3
Claudio Mauad <claudiom@microsoft.com>
2021-09-03 15:13:16 -0700

    Added retry to get all commits.

--- src/AzureDevOps.DataIngestor.Sdk/Clients/VssGitClient.cs
+++ src/AzureDevOps.DataIngestor.Sdk/Clients/VssGitClient.cs
@@ -20,8 +20,10 @@ namespace AzureDevOps.DataIngestor.Sdk.Clients
             this.retryAfter = VssClientHelper.GetRetryAfter(this.LastResponseContext);
         }
 
-        public async Task<List<GitCommitRef>> GetCommitsWithRetryAsync(Guid repositoryId, string branchName, DateTime fromDate, DateTime toDate, int top = 100, int? skip = null)
+        public async Task<List<GitCommitRef>> GetCommitsWithRetryAsync(Guid repositoryId, string branchName, DateTime fromDate, DateTime toDate, int top = 250, int? skip = 0)
         {
+            List<GitCommitRef> commitRefs = new List<GitCommitRef>();
+            
             GitQueryCommitsCriteria searchCriteria = new GitQueryCommitsCriteria
             {
                 FromDate = fromDate.ToUniversalTime().ToString("o"),
@@ -37,21 +39,34 @@ namespace AzureDevOps.DataIngestor.Sdk.Clients
                 };
             }
 
-            List<GitCommitRef> commitRefs = new List<GitCommitRef>();
-            try
+            List<GitCommitRef> currentCommiRefsAll = new List<GitCommitRef>();
+            do
             {
-                commitRefs = await RetryHelper.SleepAndRetry(this.retryAfter, this.logger, async () =>
+                try
                 {
-                    return await this.GetCommitsAsync(repositoryId, searchCriteria, skip, top);
-                });
-            }
-            catch (VssServiceException ex)
-            {
-                if (ex.Message.StartsWith("TF401019", StringComparison.OrdinalIgnoreCase))
-                {
-                    this.logger.LogWarning("Repository '{repositoryId}' does not exist, is disabled or you do not have permissions for the operation", repositoryId);
+                    currentCommiRefsAll = await RetryHelper.SleepAndRetry(this.retryAfter, this.logger, async () =>
+                    {
+                        return await this.GetCommitsAsync(repositoryId, searchCriteria, skip, top);
+                    });
                 }
-            }
+                catch (VssServiceException ex)
+                {
+                    if (ex.Message.StartsWith("TF401019", StringComparison.OrdinalIgnoreCase))
+                    {
+                        this.logger.LogWarning("Repository '{repositoryId}' does not exist, is disabled or you do not have permissions for the operation", repositoryId);
+                    }
+                }
+
+                if (currentCommiRefsAll.Count > 0)
+                {
+                    commitRefs.AddRange(currentCommiRefsAll);
+
+                }
+
+                // Next set
+                skip = skip + top;
+
+            } while (currentCommiRefsAll.Count > 0);
 
             return commitRefs;
         }
